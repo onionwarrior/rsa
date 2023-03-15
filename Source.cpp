b9d25c180d5f89a98b1124c0c10ae03c9633c666
onionwarrior <onionwarrior@mail.ru>
2023-03-15 20:05:05 +0300

    Typo fix, add utils for rsa encoding

--- Source.cpp
+++ Source.cpp
@@ -36,7 +36,7 @@ requires std::is_unsigned_v<T>
 auto make_signed_span(std::span<T> s)
 {
     using signed_t  = std::make_signed_t<T>;
-    return std::span<signed_t>{reinterpret_cast<signed_t*>(s.data()), s.size()});
+    return std::span<signed_t>{reinterpret_cast<signed_t*>(s.data()), s.size()};
 }
 
 template <typename Bytes,typename BigInt>
@@ -77,9 +77,40 @@ auto bit_length(const BigInt& v) -> std::size_t
 {
     return mp::msb(v) + 1;
 }
+namespace utils
+{
+    template <typename T>
+    using helper = std::remove_cvref_t<typename T::value_type>;
 
+    template <typename Destination,typename ... Ts>
+    requires std::conjunction_v<std::is_same<helper<Ts>, std::uint8_t> ... >
+        && std::is_same_v<std::uint8_t,typename Destination::value_type>
+
+    auto dump_to(const Ts &...seqs)->Destination
+    {
+        std::size_t size = 0;
+        auto add = [&size](auto&&val){ size += std::size(val); };
+        std::apply(
+            [&size,&add](auto &&...t) { (add(t),...); },
+            std::make_tuple(seqs...)
+        );
+        auto dst = Destination(size);
+        std::size_t push_index = 0;
+        auto push_elem = [&dst,&push_index](auto&&elem){ 
+            std::copy(std::begin(elem),std::end(elem),std::next(std::begin(dst),push_index));
+            push_index+=std::size(elem);
+        };
+        std::apply(
+            [&dst, &push_elem](auto &&...t) { (push_elem(t), ...); },
+            std::make_tuple(seqs...)
+        );
+        return dst;
+    }
+}
 namespace rsa
 {
+    const auto p = u256{ "57896044618658097711785492504343953926634992332820282019728792003956564820109" };
+    const auto q = u256{ "57896044618658097711785492504343953926634992332820282019728792003956564820063" };
     struct pub
     {
         u512 exp;
@@ -115,6 +146,10 @@ namespace rsa
 }
 namespace asn
 {
+    enum tag : std::uint8_t
+    {
+        sequence = 0x30
+    };
     //closest to 1008 required bytes, soo
     using len_holder_t = mp::checked_uint1024_t;
     using len_bytes_t = std::array<std::uint8_t, 128>;
@@ -131,6 +166,14 @@ namespace asn
             std::copy_n(std::begin(vec),size,std::begin(bytes));
             return bytes;
         }
+        using bytes =std::vector<std::uint8_t>;
+    }
+    template <typename BigInt>
+    auto byte_size(const BigInt& val) -> std::size_t 
+    {
+        const auto length_bits = bit_length(val);
+        const auto length_bytes = length_bits / 8 + (length_bits % 8 != 0);
+        return length_bytes;
     }
     auto encode_len(const len_holder_t& len) -> len_encoded_t
     {
@@ -138,8 +181,7 @@ namespace asn
         {
             return {1,{static_cast<std::uint8_t>(len)}};
         }
-        const auto length_bits = bit_length(len);
-        const auto length_bytes = length_bits / 8 + (length_bits  % 8 != 0 );
+        const auto length_bytes = byte_size(len);
         return { length_bytes,encode_wrapper(len,length_bytes)};
     }
     auto decode_len(std::span<std::uint8_t> as_bytes) ->  len_holder_t
@@ -151,12 +193,29 @@ namespace asn
         }
         return to_bigint<len_holder_t>(make_signed_span(as_bytes));
     }
-    auto encode_rsa(rsa::pub&pub) -> std::vector<std::uint8_t>
+    /*seq
+    * Return a SEQUENCE 
+    */
+    auto encode_rsa(rsa::pub&pub) -> bytes
     {
-    
+        auto&&[exp,mod] = pub;
+        auto&&[exp_len,exp_len_as_bytes] = encode_len(exp);
+        auto&&[mod_len,mod_len_as_bytes] = encode_len(mod);
+        //Size of everything except SEQUENCE len and tag
+        const auto inner_size = sizeof(tag) * 2 + byte_size(exp) + byte_size(mod) + exp_len + mod_len;
+        auto&&[encoded_seq_len,encoded_seq_len_as_bytes] = encode_len({ inner_size});
+        const auto required_size = sizeof(tag) + inner_size + encoded_seq_len;
+        auto encoded = std::vector<std::uint8_t>(required_size);
+        const auto exp_as_bytes = to_bytes<bytes>(exp);
+        const auto mod_as_bytes = to_bytes<bytes>(mod);
+        return utils::dump_to<bytes>(
+            bytes{0x30}, encoded_seq_len_as_bytes,
+            bytes{0x02},mod_len_as_bytes,mod_as_bytes,
+            bytes{0x02}, exp_len_as_bytes, exp_as_bytes
+        );
     }
 }
-namespace io
+namespace file_io
 {
     namespace
     {
@@ -181,12 +240,9 @@ auto main(int argc,char**argv) -> int
 {
     u512 n;
     u512 phi_n;
-    const auto file = read_bytes("file.txt").value_or(std::vector<std::uint8_t>{});
-    const auto p = u256{"57896044618658097711785492504343953926634992332820282019728792003956564820109"};
-    const auto q = u256{"57896044618658097711785492504343953926634992332820282019728792003956564820063"};
 
-    mp::multiply(n,p,q);
-    mp::multiply(phi_n,p-1,q-1);
+    mp::multiply(n,rsa::p,rsa::q);
+    mp::multiply(phi_n,rsa::p-1,rsa::q-1);
 
 
     const auto e = u512{"1234567"};
